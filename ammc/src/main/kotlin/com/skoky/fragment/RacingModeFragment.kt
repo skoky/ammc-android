8a091bbc92906d49b0910877c69bd12e4d50935c
Skokys <skokys@gmail.com>
2018-12-21 14:17:52 +0100

    racing mode startup delay

--- ammc/src/main/kotlin/com/skoky/fragment/RacingModeFragment.kt
+++ ammc/src/main/kotlin/com/skoky/fragment/RacingModeFragment.kt
@@ -16,6 +16,7 @@ import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import com.skoky.MainActivity
 import com.skoky.MyApp
 import com.skoky.R
 import com.skoky.Tools
@@ -174,6 +175,42 @@ class RacingModeFragment : FragmentCommon() {
     private lateinit var clock: Future<Unit>
 
     private fun doStart() {
+
+        val ma = (activity as MainActivity)
+        val startupDelay = ma.getStartupDelayFlag()
+
+        if (startupDelay) {
+            doStartDelay(ma.getStartupDelayValueFlag())
+        } else {
+            doStartNow()
+        }
+    }
+
+    private fun doStartDelay(delaySecs: Int) {
+        (timingContentView.adapter as RacingModeRecyclerViewAdapter).clearResults()
+        startStopButtonM?.text = getText(R.string.stop)
+
+        val delayStartTime = System.currentTimeMillis()
+        clock = doAsync {
+            while (System.currentTimeMillis() - delayStartTime < delaySecs * 1000) {
+                val diffSecs = (System.currentTimeMillis() - delayStartTime) / 1000
+                val time = delaySecs - diffSecs
+                val str = "Start in ${time}s"
+                uiThread {
+                    clockViewX.text = str
+                }
+                Thread.sleep(30)
+            }
+
+            uiThread {
+                doStartNow()
+            }
+        }
+
+    }
+
+    private fun doStartNow() {
+
         (timingContentView.adapter as RacingModeRecyclerViewAdapter).clearResults()
         running = true
         startStopButtonM?.text = getText(R.string.stop)
